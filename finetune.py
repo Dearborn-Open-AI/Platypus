8421b826c05aa3eabdd1236a859bda1cc3a900c1
mshumer <41550495+mshumer@users.noreply.github.com>
2023-08-18 19:22:59 -0400

    adjust dataset type

--- finetune.py
+++ finetune.py
@@ -178,30 +178,23 @@ def train(
         return result
 
     def generate_and_tokenize_prompt(data_point):
-        full_prompt = prompter.generate_prompt(
-            data_point["instruction"],
-            data_point["input"],
-            data_point["output"])
-        
-        tokenized_full_prompt = tokenize(full_prompt)
-        if not train_on_inputs:
-            user_prompt = prompter.generate_prompt(
-                data_point["instruction"], data_point["input"])
-            
-            tokenized_user_prompt = tokenize(
-                user_prompt, add_eos_token=add_eos_token)
-            
-            user_prompt_len = len(tokenized_user_prompt["input_ids"])
+      full_prompt = data_point["prompt"] + data_point["response"]
+          
+      tokenized_full_prompt = tokenize(full_prompt)
+      if not train_on_inputs:
+          tokenized_user_prompt = tokenize(data_point["prompt"], add_eos_token=add_eos_token)
+              
+          user_prompt_len = len(tokenized_user_prompt["input_ids"])
 
-            if add_eos_token:
-                user_prompt_len -= 1
+          if add_eos_token:
+              user_prompt_len -= 1
 
-            tokenized_full_prompt["labels"] = [
-                -100
-            ] * user_prompt_len + tokenized_full_prompt["labels"][
-                user_prompt_len:
-            ]  # TODO: Speed up?
-        return tokenized_full_prompt
+          tokenized_full_prompt["labels"] = [
+              -100
+          ] * user_prompt_len + tokenized_full_prompt["labels"][
+              user_prompt_len:
+          ]  # TODO: Speed up?
+      return tokenized_full_prompt
 
     model = prepare_model_for_int8_training(model)
 
@@ -216,7 +209,7 @@ def train(
     model = get_peft_model(model, config)
 
     if data_path.endswith(".json") or data_path.endswith(".jsonl"):
-        data = load_dataset("json", data_files=data_path)
+        data = load_dataset('json', data_files=data_path, split='train')
     else:
         data = load_dataset(data_path)
 
